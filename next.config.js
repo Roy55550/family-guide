f25b00f85c3d1a6028fd9db9923d9d5aa09f54bc
roy55550 <roy55550@gmail.com>
2024-10-05 14:37:48 +0300

    eli fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: [
-      'i.imgur.com',
-      'images.unsplash.com',  // Unsplash images
-      'res.cloudinary.com',   // Cloudinary images
-      'hebbkx1anhila5yf.public.blob.vercel-storage.com',  // Vercel storage
-      'i.pravatar.cc'         // Pravatar for avatarss
-      // Add any other domains you need here
-    ],
+    unoptimized: true,
   },
-};
+}
 
 module.exports = nextConfig
