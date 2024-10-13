921807a1276ae728a631b7187910de6d9fd5479a
roy55550 <roy55550@gmail.com>
2024-10-13 12:48:30 +0300

    Updated article content

---
+++ app/articles/divorce-process-guide/how-to-navigate-divorce-smoothly/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HowToNavigateDivorceSmoothlyArticle from '../how-to-navigate-divorce-smoothly';
+
+const ArticlePage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-[#FFE8D6] py-8">
+      <HowToNavigateDivorceSmoothlyArticle />
+    </div>
+  );
+};
+
+export default ArticlePage;
